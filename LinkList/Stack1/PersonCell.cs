60e24577ef0c6508982cc5adeda088b3019586cd
Ponhrith <ponhrith.nhep@gmail.com>
2023-03-21 16:57:15 +0700

    Stack Done

---
+++ LinkList/Stack1/PersonCell.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack1
+{
+    internal class PersonCell
+    {
+        public string Name;
+        public PersonCell Next; //Next contains the address of the PersonCell obj
+
+        public PersonCell() { }
+
+        public PersonCell(string name, PersonCell next)
+        {
+            Name = name;
+            Next = next;
+        }
+
+        public void push(string newName)
+        { //newCell is a new node
+            PersonCell newCell = new PersonCell();
+            newCell.Name = newName;
+            newCell.Next = this.Next;
+            this.Next = newCell;
+        }
+
+        public void pop(PersonCell TopCell)
+        {
+            PersonCell FirstCell = TopCell.Next;
+            TopCell.Next = FirstCell.Next;
+        }
+
+    }
+}
